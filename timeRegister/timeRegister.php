44948a1f8e1e64c75ac075f06ee4a7873fb2a7c7
uchikosi <ucchikohiwithsugar@gmail.com>
2024-03-30 14:14:58 +0900

    勤務登録機能、送信時のメッセージ表示位置変更

--- timeRegister/timeRegister.php
+++ timeRegister/timeRegister.php
@@ -1,18 +1,5 @@
 <?php
 session_start();
-// メッセージが渡されたかどうかを確認
-if (isset($_GET['message'])) {
-  // メッセージを取得
-  $message = $_GET['message'];
-
-  // メッセージに応じて表示を変更
-  if ($message == "success") {
-    echo "お疲れ様でした、勤怠情報が正常に送信されました。";
-  } elseif ($message == "error") {
-    echo "エラー：勤怠情報を送信できませんでした。";
-  }
-}
-
 // データベースへの接続
 mb_internal_encoding("utf8");
 $servername = "localhost";
@@ -93,49 +80,63 @@ try {
       <li class="nav"><a href="../logout.php" id="logout">Logout</a></li>
     </ul>
   </header>
+  <main>
+    <!-- 登録 -->
+    <h2>勤怠入力</h2>
+    <div>
+      <form action="timeRegisterComplete.php" method="post" id="attendanceForm">
 
-  <!-- 登録 -->
-  <h2>勤怠入力</h2>
-  <div>
-    <form action="timeRegisterComplete.php" method="post" id="attendanceForm">
+        <label>年月日:</label>
+        <input type="date" min="2024-01" name="date" id="selectedDate" value="">
+        <!-- <br> -->
 
-      <label>年月日:</label>
-      <input type="date" min="2024-01" name="date" id="selectedDate" value="">
-      <!-- <br> -->
+        <label>区分:</label>
+        <select name="category" id="categorySelect" onchange="handleCategoryChange()">
+          <option value="" selected disabled>選択してください</option>
+          <option value="holiday">公休</option>
+          <option value="going_to_work">出勤</option>
+          <option value="absence">欠勤</option>
+          <option value="paid">有給</option>
+          <option value="holiday_work">休日出勤</option>
+          <option value="behind_time">遅刻</option>
+          <option value="leaving_early">早退</option>
+        </select><br>
 
-      <label>区分:</label>
-      <select name="category" id="categorySelect" onchange="handleCategoryChange()">
-        <option value="" selected disabled>選択してください</option>
-        <option value="holiday">公休</option>
-        <option value="going_to_work">出勤</option>
-        <option value="absence">欠勤</option>
-        <option value="paid">有給</option>
-        <option value="holiday_work">休日出勤</option>
-        <option value="behind_time">遅刻</option>
-        <option value="leaving_early">早退</option>
-      </select><br>
+        <label for="start_time" id="startTimeLabel">出勤時間:</label>
+        <input type="time" id="start_time" name="start_time" onchange="calculateWorkTime()">
+        <!-- <br> -->
 
-      <label for="start_time" id="startTimeLabel">出勤時間:</label>
-      <input type="time" id="start_time" name="start_time" onchange="calculateWorkTime()">
-      <!-- <br> -->
+        <label for="end_time" id="endTimeLabel">退勤時間:</label>
+        <input type="time" id="end_time" name="end_time" onchange="calculateWorkTime()"><br>
 
-      <label for="end_time" id="endTimeLabel">退勤時間:</label>
-      <input type="time" id="end_time" name="end_time" onchange="calculateWorkTime()"><br>
+        <label for="break_time" id="breakTimeLabel">休憩時間:</label>
+        <input type="time" id="break_time" name="break_time" onchange="calculateWorkTime()">
+        <!-- <br> -->
 
-      <label for="break_time" id="breakTimeLabel">休憩時間:</label>
-      <input type="time" id="break_time" name="break_time" onchange="calculateWorkTime()">
-      <!-- <br> -->
+        <label for="standard_working_time" id="standardWorkingTimeLabel">実働時間:</label>
+        <input type="time" id="standard_working_time" name="standard_working_time" readonly><br>
+        <label for="over_time" id="overTimeLabel">残業時間:</label>
+        <input type="time" id="over_time" name="over_time">
+        <!-- <br> -->
 
-      <label for="standard_working_time" id="standardWorkingTimeLabel">実働時間:</label>
-      <input type="time" id="standard_working_time" name="standard_working_time" readonly><br>
-      <label for="over_time" id="overTimeLabel">残業時間:</label>
-      <input type="time" id="over_time" name="over_time">
-      <!-- <br> -->
+        <input type="submit" value="送信">
+      </form>
+      <p><a href="../top.php">TOPへ戻る</a></p>
+    </div>
+    <?php
+      // メッセージが渡されたかどうかを確認
+    if (isset($_GET['message'])) {
+      // メッセージを取得
+      $message = $_GET['message'];
 
-      <input type="submit" value="送信">
-    </form>
-    <p><a href="../top.php">TOPへ戻る</a></p>
-  </div>
+      // メッセージに応じて表示を変更
+      if ($message == "success") {
+        echo "お疲れ様でした、勤怠情報が正常に送信されました。";
+      } elseif ($message == "error") {
+        echo "エラー：勤怠情報を送信できませんでした。";
+      }
+    }?>
+  </main>
 
     <!-- 表示 -->
   <?php
